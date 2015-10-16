1bb65c4b62771355a2f78670d92b9fadbfabd80f
senthilshravan <senthilshravanbala@gmail.com>
2015-10-16 09:00:09 +0530

    Added SRT Conversion

--- yandex.py
+++ yandex.py
@@ -4,7 +4,6 @@ import requests.exceptions
 
 class YandexTranslator(object):
     url = "https://translate.yandex.net/api/v1.5/tr.json/{endpoint}?"
-    # =trnsl.1.1.20151011T175944Z.f6a3de65607a853a.1899ccefa5b8eaf25acc983905132b5212f47a37"
     key = ""
     function = ""
     dict = {"langs": "getLangs", "det": "detect", "trans": "translate"}
@@ -47,5 +46,41 @@ class YandexTranslator(object):
         except Exception as e:
             print(type(e))
 
+    def convert(self, file):
+        k = 10
+        j = 0
+        f = open(file, "r+")
+        newfile = open("convertedfile.srt", "w+")
+        # print(sentence)
+        page = f.read()
+        i = 0
+        for sentence in page.splitlines():
+            # Adding the timeline
+            if j > k:
+                break
+            if i is 1:
+                newfile.write(sentence)
+                i = 0
+                newfile.write("\n")
+            else:
+                flag = 0
+                try:
+                    digit = int(sentence)
+                except ValueError:
+                    flag = 1
+                # Adding numbers
+                if flag is 0:
+                    i = 1
+                    j = j + 1
+                    newfile.write(sentence)
+                    newfile.write("\n")
+                    print(sentence)
+                # Adding sentence
+                if flag is 1:
+                    newfile.write(self.trans("de", sentence, options=None)[0])
+                    newfile.write("\n")
+        f.close()
+        newfile.close()
+
 yt = YandexTranslator("trnsl.1.1.20151011T175944Z.f6a3de65607a853a.1899ccefa5b8eaf25acc983905132b5212f47a37")
-print(yt.trans("ja", "what do you mean", 1))
+yt.convert("YOUR SRT FILE")
